7fefa0edaf12060062b317b8647a6033e279958d
Akvermacert <159696239+Akvermacert@users.noreply.github.com>
2024-08-01 16:13:36 +0000

    done

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,29 @@
-console.log("hello");
+//console.log("hello");
+let a =5;
+const B=56;
+const studnet={
+    fullname: 'Akash',
+    age: 20,
+    cgpa:7.4,
+    isPass:true,
+};
+const product={
+    title: "Ball pen",
+    rating:4,
+    offer: 5,
+    price: 270,
+};
+
+//console.log(product);
+const profile={
+    username: "@AkashVerma",
+    isFollow: true,
+    following:123,
+    followers:543,
+};
+let num= prompt("enter a number:");
+if(num%5===0){
+    console.log(num,"is a multiple of 5");
+}else{
+    console.log(num,"is not multiple of 5");
+}
